1c56b97da6e45e49813460ac70bbbc1ec373edfb
GigaChad12345-code <millemor007@alpinesd.org>
2026-04-30 19:28:33 -0600

    Update wisp.client.js

--- public/wisp.client.js
+++ public/wisp.client.js
@@ -1,1 +1,8 @@
+// Wisp client loader
+navigator.serviceWorker.register("/wisp.sw.js");
+
+async function connectWisp(url) {
+  const encoded = btoa(url);
+  window.location.href = "/wisp/" + encoded;
+}
 
