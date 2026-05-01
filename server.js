bd042825d374691b46d951055bc97fc4c608d54a
GigaChad12345-code <millemor007@alpinesd.org>
2026-04-30 19:27:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
-const { createBareServer } = require("bare-server-node");
-const { uvPath } = require("@titaniumnetwork-dev/ultraviolet");
+const { createServer } = require("@mercuryworkshop/wisp-server");
 
 const app = express();
-const server = http.createServer(app);
-const bare = createBareServer("/bare/");
+const wisp = createServer();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uv/", express.static(uvPath));
 
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
+app.use("/wisp/", wisp.app);
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log("NullForge running on port " + PORT);
+app.listen(PORT, () => {
+  console.log("Wisp proxy running on port " + PORT);
 });
